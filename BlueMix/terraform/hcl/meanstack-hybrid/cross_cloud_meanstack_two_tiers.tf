092c362b9f654f51288b8bd0f8299f23f6c08f24
CAMHub PublishBot <octravis@us.ibm.com>
2019-12-09 22:42:08 +0000

    Automated CAMHub::publish on 2019-12-09 22:42:08 UTC

--- BlueMix/terraform/hcl/meanstack-hybrid/cross_cloud_meanstack_two_tiers.tf
+++ BlueMix/terraform/hcl/meanstack-hybrid/cross_cloud_meanstack_two_tiers.tf
@@ -22,7 +22,7 @@
 # Define the AWS provider
 #########################################################
 provider "aws" {
-  version    = "~> 1.2"
+  version = "~> 2.0"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
