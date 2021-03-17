2fad283101c6225e17762bf88d2138755c0c8e0d
CAMHub PublishBot <octravis@us.ibm.com>
2021-03-17 15:34:40 +0000

    Automated CAMHub::publish on 2021-03-17 15:34:40 UTC

--- Azure/terraform/hcl/singlevirtualmachine/main.tf
+++ Azure/terraform/hcl/singlevirtualmachine/main.tf
@@ -57,7 +57,6 @@ variable "admin_user_password" {
 
 variable "user_public_key" {
   description = "Public SSH key used to connect to the virtual machine"
-  default     = "None"
 }
 
 #########################################################
@@ -175,7 +174,6 @@ resource "azurerm_storage_container" "default" {
 #########################################################
 resource "azurerm_virtual_machine" "vm" {
   depends_on			      = [azurerm_network_interface_security_group_association.vm]
-  count                 = var.user_public_key != "None" ? 1 : 0
   name                  = "${var.name_prefix}-vm"
   location              = var.azure_region
   resource_group_name   = azurerm_resource_group.default.name
